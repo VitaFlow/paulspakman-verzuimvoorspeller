d612164d0a8a493854663b072d8ccbf83187e704
VitaFlow <info@optivita.store>
2025-06-23 21:57:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import joblib
 
 # Modellen en data inladen
 df = pd.read_excel("verzuimdata_demo_met_verzuim.xlsx")
-clf_model = joblib.load("model_classification.pkl")
-reg_model = joblib.load("model_regression.pkl")
-model_features = joblib.load("model_features.pkl")
+clf_model = joblib.load("model_classification_streamlit13.pkl")
+reg_model = joblib.load("model_regression_streamlit13.pkl")
+features = joblib.load("model_features_streamlit13.pkl")
 
 # Voorbereiden features
 def prepare_input(data):
